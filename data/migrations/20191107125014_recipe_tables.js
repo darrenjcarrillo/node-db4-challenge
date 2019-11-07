3a208dc21f2c48bd38239b5e98fb1da6191c9bec
Darren J Carrillo <darrenjcarrillo@gmail.com>
2019-11-07 13:20:15 -0800

    Created knex, and migration

---
+++ data/migrations/20191107125014_recipe_tables.js
@@ -0,0 +1,68 @@
+exports.up = function(knex) {
+  return knex.schema
+    .createTable("quantity", tbl => {
+      // the type of the Primary Key is: integer without negative values, also called unsigned
+      tbl.increments();
+      tbl.float("quantity").notNullable();
+      tbl.string("units", 255);
+    })
+    .createTable("ingredients", tbl => {
+      tbl.increments();
+
+      tbl.string("name", 255).notNullable();
+
+      // define Foreign Key
+      tbl
+        .integer("quantity_id")
+        .unsigned()
+        .references("id")
+        .inTable("quantity")
+        .onDelete("RESTRICT") // about deleting the record from the primary key table. Could be 'RESTRICT', 'NO ACTION', 'SET NULL'
+        .onUpdate("CASCADE"); // about changing the value of the primary key table.
+    })
+    .createTable("recipes", tbl => {
+      tbl.increments();
+
+      tbl.string("name", 255).notNullable();
+    })
+    .createTable("instructions", tbl => {
+      tbl.increments();
+      tbl.string("step", 255).notNullable();
+    })
+
+    .createTable("recipe_ingredients", tbl => {
+      tbl.increments();
+      tbl.string("from", 255);
+
+      tbl
+        .integer("recipe_id")
+        .unsigned()
+        .references("id")
+        .inTable("recipes")
+        .onDelete("RESTRICT")
+        .onUpdate("CASCADE");
+      tbl
+        .integer("ingredients_id")
+        .unsigned()
+        .references("id")
+        .inTable("ingredients")
+        .onDelete("RESTRICT")
+        .onUpdate("CASCADE");
+      tbl
+        .integer("instruction_id")
+        .unsigned()
+        .references("id")
+        .inTable("instruction")
+        .onDelete("RESTRICT")
+        .onUpdate("CASCADE");
+    });
+};
+
+exports.down = function(knex) {
+  return knex.schema
+    .dropTableIfExists("recipe_ingredients")
+    .dropTableIfExists("instructions")
+    .dropTableIfExists("recipes")
+    .dropTableIfExists("ingredients")
+    .dropTableIfExists("quantity");
+};
